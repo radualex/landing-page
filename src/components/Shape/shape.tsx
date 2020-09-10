bffd98657408e6e9196ed8f223090d2f819755d7
Radu Stoica <radu.stoica1994@gmail.com>
2020-09-10 20:20:43 +0200

    finished shapes

--- src/components/Shape/shape.tsx
+++ src/components/Shape/shape.tsx
@@ -5,6 +5,8 @@ export default function Shape() {
   return (
     <div className="shape-wrapper">
       <div className="triangle-bottomRight"></div>
+      <div className="rounded-rectangle"></div>
+      <div className="half-circle"></div>
     </div>
   );
 }
